815b9005ff1f4d1773e6fa2e4c522bd78c1c550f
cryp70m4n <cryp70m4n@gmail.com>
2022-09-17 09:23:22 +0000

    added tailwind

---
+++ server_side/static/js/index.js
@@ -0,0 +1,13 @@
+let i = 0;
+let txt = 'Welcome to Xena systems';
+let speed = 50;
+function titleWriter() {
+	if (i < txt.length) {
+		document.getElementById("title").innerHTML += txt.charAt(i);
+		console.log(txt.charAt(i))
+		i++;
+		setTimeout(titleWriter, speed);
+	}
+}
+
+titleWriter()
